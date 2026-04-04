77907227f24d926e4a6316f3b88eb654b924e637
Vigneshwaran <vigneshwaran.mtech@gmail.com>
2026-04-04 22:35:17 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <div style={styles.appContainer}>
       <ContactForm />
       <LeadsList leads={dummyLeads} />
+      {/* <LeadsList leads={dummyLeads} /> */}
     </div>
   );
 }
